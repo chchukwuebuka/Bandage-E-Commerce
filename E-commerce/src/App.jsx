52a38a01c3adbf9735ee9ca6dfa820b8b8f4839a
chukwuebuka <emmanuel.henry71@gmail.com>
2024-05-30 23:45:20 +0100

    added few things in my productpage

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import './product.css'
 import Landing from './componenet/landing'
+import Product from './componenet/product'
 
 function App() {
  
 
   return (
-    <>
-     < Landing /> 
-    </>
+
+     (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </Router>
+    )
+   
   )
 }
 
